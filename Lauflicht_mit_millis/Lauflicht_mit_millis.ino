4f7f910c572b57e582a15a24e6e621ef35b9417a
KK918 <71316344+KK918@users.noreply.github.com>
2020-10-26 18:26:11 +0100

    Update Lauflicht_mit_millis.ino

--- Lauflicht_mit_millis/Lauflicht_mit_millis.ino
+++ Lauflicht_mit_millis/Lauflicht_mit_millis.ino
@@ -1,41 +1,39 @@
 int led[] {2, 3, 4, 5, 6, 7, 8, 9};
-unsigned long  intervall = 100;
+unsigned long  intervall = 500;
 unsigned long  start;
 unsigned long  ende;
 boolean ledZustand = false;
-int z;
+int z = -1;
 
 
 void setup()
 {
- for (int i = 0; i < 8; i++)
+  for (int i = 0; i < 8; i++)
   {
     pinMode(led[i], OUTPUT);
-   }
+  }
 
 }
 
 void loop()
 {
 
- {
-  if (millis() - start >= intervall)
- 
   {
-    ledZustand = !ledZustand;
-    start = millis();
-z++;
-    
-     }
- 
- 
- {
-   digitalWrite(led[z], ledZustand);
-  
- }
- }
+    if (millis() - start >= intervall)
+
+    {
+      ledZustand = !ledZustand;
+      start = millis();
+      z = z+1;
+
+    }
+
+
+    {
+      digitalWrite(led[z], ledZustand);
+
+
+    }
+  }
 
 }
-
-
-   
